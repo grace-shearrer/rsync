a6c1e273ef30b390c9f0dca5036f420d20e36424
grace-shearrer <gshearre@login4.ls4.tacc.utexas.edu>
2014-09-26 15:57:09 -0500

    backing up freesurfer scripts

---
+++ freesurfer_scripts/fs_edits.sh
@@ -0,0 +1,2 @@
+recon-all -no-isrunning -autorecon2 -autorecon3 -sd 
+/corral-repl/utexas/poldracklab/data/sugar_brain/freesurfer -subjid sugar_brain_sb_006
---
+++ freesurfer_scripts/fs_recon1.sh
@@ -0,0 +1,1 @@
+recon-all -no-isrunning -autorecon1 -sd /corral-repl/utexas/poldracklab/data/sugar_brain/freesurfer -subjid <sb_0##>
